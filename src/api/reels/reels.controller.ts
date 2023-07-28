56ce04015b2443a1f58fe1a5b083578b6c4eb7c4
Jatin Kumar <main.jatink@gmail.com>
2023-07-28 20:44:40 +0530

    feat: get & create reels

--- src/api/reels/reels.controller.ts
+++ src/api/reels/reels.controller.ts
@@ -1,7 +1,78 @@
-import { Request, Response } from 'express';
+import { db } from '@/loaders/database';
+import { uploadObject } from '@/shared/services/upload';
+import { NextFunction, Request, Response } from 'express';
 
-export const getReels = (req: Request, res: Response) => {
-  res.json({
-    name: 'Hello form reels',
-  });
+export const getReels = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { data, error } = await (await db()).from('reels').select();
+
+    if (error) {
+      throw {
+        statusCode: 400,
+        message: error.message,
+      };
+    }
+
+    res.json({
+      success: true,
+      data,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createReel = async (
+  req: Request & {
+    file: Express.Multer.File;
+  },
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const objectUrl = await uploadObject(req.file);
+
+    const token = req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+      throw {
+        statusCode: 400,
+        message: 'No token provided',
+      };
+    }
+
+    // Get the user from the token
+    const {
+      data: { user },
+      error: userError,
+    } = await (await db()).auth.getUser(token);
+
+    if (userError) {
+      throw {
+        statusCode: 400,
+        message: userError?.message,
+      };
+    }
+
+    // Create a new post in the "posts" table
+    const { error: dbError } = await (await db()).from('reels').insert({
+      created_by: user?.id,
+      video_url: objectUrl,
+      description: req.body.description,
+      likes: 0,
+      dislikes: 0,
+      bookmarks: 0,
+    });
+
+    if (dbError) {
+      throw {
+        statusCode: 400,
+        message: dbError?.message,
+      };
+    }
+
+    return res.json({ success: true, message: 'Reel created successfully' });
+  } catch (err) {
+    next(err);
+  }
 };
--- src/api/reels/reels.router.ts
+++ src/api/reels/reels.router.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
-import { getReels } from './reels.controller';
+import { createReel, getReels } from './reels.controller';
+import { MIME_TYPE, upload } from '@/shared/middlewares/multer';
 
 export default (): Router => {
   const app = Router();
 
   app.get('/', getReels);
+  app.post('/create', upload(MIME_TYPE.VIDEO).single('video'), createReel);
 
   return app;
 };
