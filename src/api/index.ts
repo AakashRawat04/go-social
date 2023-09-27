034988674a2c5e7d79b774230d608877c0d2bc46
Ash <01tripster@gmail.com>
2023-09-27 10:43:33 +0530

    feat: add login route

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,6 +4,7 @@ import userRouter from './users/user.router';
 import reelsRouter from './reels/reels.router';
 import messagesRouter from './messages/messages.router';
 import authenticateToken from '../shared/middlewares/authentication';
+import { db } from '@/loaders/database';
 
 export default (): Router => {
   const app = Router();
@@ -13,5 +14,28 @@ export default (): Router => {
   app.use('/reels', authenticateToken(), reelsRouter());
   app.use('/messages', authenticateToken(), messagesRouter());
 
+  app.use('/login', async (req, res, next) => {
+    try {
+      const { email, password } = req.body;
+      console.log(email, password);
+
+      const { data, error } = await (await db()).auth.signInWithPassword({ email, password });
+
+      if (error) {
+        throw {
+          statusCode: 400,
+          message: error.message,
+        };
+      }
+      const token = data.session.access_token;
+
+      res.json({
+        success: true,
+        token,
+      });
+    } catch (error) {
+      next(error);
+    }
+  });
   return app;
 };
